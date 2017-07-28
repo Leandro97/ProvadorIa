97357a13e1b28161a695b8d0bebe3036cbde7fc5
Leandro97 <leandro97.martins@gmail.com>
2017-07-28 10:29:54 -0300

    Provador pronto

--- ProvadorIa/src/Main.java
+++ ProvadorIa/src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-	public static boolean testaP(String texto) {
+	static ArrayList<Character> retorno = new ArrayList<Character>();
+	public static boolean testa(String texto) {
 		//String atomos = "[ \\t\\n\\x0B\\f\\r]?[a-zA-Z][ \\t\\n\\x0B\\f\\r]?";
 		String atomo = "[a-zA-Z]";
 		String expressao = "[a-zA-Z]->[a-zA-Z]";
@@ -16,29 +17,34 @@ public class Main {
 		return false;
 	}
 	
-	public static boolean testaC(String texto) {
-		//String atomos = "[ \\t\\n\\x0B\\f\\r]?[a-zA-Z][ \\t\\n\\x0B\\f\\r]?";
-		String atomo = "[a-zA-Z]";
-		
-		if(Pattern.matches(atomo, texto)) {
-			return true;
+	public static void geraConhecimento(ArrayList<String> atomos, ArrayList<String> expressoes) {
+		for(int i = 0; i < atomos.size(); i++) {
+			String a = (String)atomos.get(i);
+			for(int j = 0; j < expressoes.size(); j++) {
+				String e = (String)expressoes.get(j);
+				
+				if(a.charAt(0) == e.charAt(0)) {
+					retorno.add(e.charAt(3));
+					atomos.remove(i);
+					expressoes.remove(j);
+					atomos.add(Character.toString(e.charAt(3)));
+					i--;
+				}
+			}
 		}
-		
-		return false;
 	}
 	
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
-		ArrayList atomos = new ArrayList<String>();
-		ArrayList expressoes = new ArrayList<String>();
-		ArrayList conclusoes = new ArrayList<String>();
-		ArrayList retorno = new ArrayList<String>();
+		ArrayList<String> atomos = new ArrayList<String>();
+		ArrayList<String> expressoes = new ArrayList<String>();
+		ArrayList<String> conclusoes = new ArrayList<String>();
 		
 		System.out.println("Digite as premissas (uma por linha). Digite # para terminar.");
 		for(;;) {
 			String texto = read.nextLine();
 			if(texto.equals("#")) break;
-			if(!testaP(texto)) {
+			if(!testa(texto)) {
 				System.out.println("Expressão inválida! Informe uma expressão do tipo \"A\" ou \"A->B\"");
 			} else if(texto.length() == 1){	
 				atomos.add(texto);
@@ -46,25 +52,20 @@ public class Main {
 				expressoes.add(texto);
 			}
 		}
-		
-		System.out.println("Digite as conclusões (uma por linha). Digite # para terminar.");
-		for(;;) {
-			String texto = read.nextLine();
-			if(texto.equals("#")) break;
-			if(!testaC(texto)) {
-				System.out.println("Expressão inválida! Informe uma expressão do tipo \"A\" ou \"A->B\"");
-			} else {	
-				conclusoes.add(texto);
-			}	
-				
-		}
 
 		Collections.sort(atomos.subList(0, atomos.size()));
 		Collections.sort(expressoes.subList(0, expressoes.size()));
-		Collections.sort(conclusoes.subList(0, conclusoes.size()));
-		System.out.println(atomos);
-		System.out.println(expressoes);
-		System.out.println(conclusoes);
+//		System.out.println(atomos);
+//		System.out.println(expressoes);
+		
+		geraConhecimento(atomos, expressoes);
+		
+		if(retorno.size() != 0) {
+			System.out.println("A partir das informações dadas podemos concluir que: " + retorno);
+		} else {
+			System.out.println("Não é possível chegar a uma conclusão com os dados informados.");
+		}
+		
 	}
 
 }
